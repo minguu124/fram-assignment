991973717f6cc3bcda9cb3b699fe6ab28c693c75
Minh Nguyen <minh.nguyenhoang2401@gmail.com>
2021-05-29 16:31:28 +0700

    add modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,18 +1,51 @@
-import React from "react";
-import { Modal, Spinner } from "react-bootstrap";
+import React, { useCallback } from "react";
+import {
+  Button,
+  Modal as BootstrapModal,
+  ModalProps as BootstrapModalProps
+} from "react-bootstrap";
 
-interface LoadingProp {
-  isLoading: boolean;
+interface ModalProps extends BootstrapModalProps {
+  title?: string;
+  onPrimary?: () => void;
+  onSecondary?: () => void;
+  primaryLabel?: string;
+  secondaryLabel?: string;
 }
 
-const Loading = ({ isLoading }: LoadingProp) => {
+const Modal = ({
+  title,
+  onPrimary,
+  onSecondary,
+  children,
+  primaryLabel = "Submit",
+  secondaryLabel = "Cancel"
+}: ModalProps) => {
+  const renderFooter = useCallback(() => {
+    if (!onPrimary && !onSecondary) return null;
+    return (
+      <BootstrapModal.Footer>
+        {!!onSecondary && <Button onClick={onPrimary}>{secondaryLabel}</Button>}{" "}
+        {!!onPrimary && (
+          <Button variant="primary" onClick={onPrimary}>
+            {primaryLabel}
+          </Button>
+        )}
+      </BootstrapModal.Footer>
+    );
+  }, [onPrimary, onSecondary, primaryLabel, secondaryLabel]);
+
   return (
-    <Modal show={isLoading} backdrop="static" centered>
-      <Modal.Body>
-        <Spinner animation="border" variant="primary" />
-      </Modal.Body>
-    </Modal>
+    <BootstrapModal centered>
+      {!!title && (
+        <BootstrapModal.Header closeButton>
+          <BootstrapModal.Title>{title}</BootstrapModal.Title>
+        </BootstrapModal.Header>
+      )}
+      <BootstrapModal.Body>{children}</BootstrapModal.Body>
+      {renderFooter()}
+    </BootstrapModal>
   );
 };
 
-export default Loading;
+export default Modal;
