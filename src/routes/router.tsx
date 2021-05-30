910e17e99db7b9d16259c965248838876bee6d33
Minh Nguyen <minh.nguyenhoang2401@gmail.com>
2021-05-30 12:00:17 +0700

    update router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,16 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Counter from "src/pages/Counter";
 import Users from "src/pages/Users";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Route exact path="/">
+        <Redirect to="/counter" />
+      </Route>
       <Switch>
-        <Route path="/">
+        <Route path="/counter">
           <Counter />
         </Route>
         <Route path="/users">
